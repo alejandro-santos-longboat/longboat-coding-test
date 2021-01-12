d90d2eea2bef38694dae8070dd616b2b6b8fdda1
alejandrodlsp <alejandrodlsp@hotmail.es>
2021-01-12 11:50:21 +0000

    Added bootstrap forms and styling to dashboard and landing page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,13 @@
 class DashboardController < ApplicationController
 
     def index
-        @username = session[:user_id]
+
+        if session[:user_id] == nil
+            redirect_to '/'
+            return
+        end
+        
+        @username = helpers.get_username(session[:user_id])
     end
     
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,11 @@
 module DashboardHelper
+
+    def get_user(user_id)
+        User.find(user_id)
+    end
+
+    def get_username(user_id)
+        get_user(user_id).username
+    end
+    
 end
